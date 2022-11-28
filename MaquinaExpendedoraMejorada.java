f65e46ab61d416483276c895a7bdebe932391764
Mauro <maurogutierreztormo@gmail.com>
2022-11-28 10:07:07 +0100

    nuevo constructor sin parametros

--- MaquinaExpendedoraMejorada.java
+++ MaquinaExpendedoraMejorada.java
@@ -23,6 +23,13 @@ public class MaquinaExpendedoraMejorada {
         estacionOrigen = origen;
         estacionDestino = destino;
     }
+     public MaquinaExpendedoraMejorada() {
+        precioBillete = 20;
+        balanceClienteActual = 0;
+        totalDineroAcumulado = 0;
+        estacionOrigen = "Leon";
+        estacionDestino = "Salamanca";
+    }
 
     /**
      * Devuelve el precio del billete
